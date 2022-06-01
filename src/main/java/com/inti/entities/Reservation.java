cdfc217bc3ae1b09b3a35103e22af9993419cfc4
pariadark <taillefumierbaptsite@gmail.com>
2022-06-01 14:43:18 +0200

    Ajout de la classe Reservation

---
+++ src/main/java/com/inti/entities/Reservation.java
@@ -0,0 +1,58 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Reservation implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idReservation;
+	private Date dateReservation;
+	private int nbJours;
+
+	public Reservation() {
+
+	}
+
+	public Reservation(Date dateReservation, int nbJours) {
+		this.dateReservation = dateReservation;
+		this.nbJours = nbJours;
+	}
+
+	public Long getIdReservation() {
+		return idReservation;
+	}
+
+	public void setIdReservation(Long idReservation) {
+		this.idReservation = idReservation;
+	}
+
+	public Date getDateReservation() {
+		return dateReservation;
+	}
+
+	public void setDateReservation(Date dateReservation) {
+		this.dateReservation = dateReservation;
+	}
+
+	public int getNbJours() {
+		return nbJours;
+	}
+
+	public void setNbJours(int nbJours) {
+		this.nbJours = nbJours;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [idReservation=" + idReservation + ", dateReservation=" + dateReservation + ", nbJours="
+				+ nbJours + "]";
+	}
+
+}
