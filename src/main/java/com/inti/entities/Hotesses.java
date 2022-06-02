c2e46285e193888d262c9f2564f5d86f634d00a3
MahiousI <ishakmahious@yahoo.fr>
2022-06-02 10:27:30 +0200

    ajout des entités dans les classes pilote et hotesse

--- src/main/java/com/inti/entities/Hotesses.java
+++ src/main/java/com/inti/entities/Hotesses.java
@@ -1,6 +1,16 @@
 package com.inti.entities;
 
-public class Hotesses {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hotesses implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idHotesse;
 
 }
